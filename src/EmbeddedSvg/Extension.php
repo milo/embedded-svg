227501f591f069ea026d620ccab00c4b2e38e359
Miloslav Hůla <miloslav.hula@gmail.com>
2019-04-15 18:57:50 +0200

    Nette DI v3 compatibility

--- src/EmbeddedSvg/Extension.php
+++ src/EmbeddedSvg/Extension.php
@@ -3,19 +3,24 @@
 namespace Milo\EmbeddedSvg;
 
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions;
 
 
 class Extension extends CompilerExtension
 {
 	public function loadConfiguration()
 	{
-		$this->getContainerBuilder()
-			->getDefinition('latte.latteFactory')
-				->addSetup('?->onCompile[] = function ($engine) { '
-					. Macro::class . '::install($engine->getCompiler(), '
-					. MacroSetting::class . '::createFromArray(?)'
-					. ');}',
-					['@self', $this->getConfig()]
-				);
+		$definition = $this->getContainerBuilder()->getDefinition('latte.latteFactory');
+		if (class_exists(Definitions\FactoryDefinition::class)) { # Nette DI v3 compatibility
+			$definition = $definition->getResultDefinition();
+		}
+
+		$definition
+			->addSetup('?->onCompile[] = function ($engine) { '
+				. Macro::class . '::install($engine->getCompiler(), '
+				. MacroSetting::class . '::createFromArray(?)'
+				. ');}',
+				['@self', $this->getConfig()]
+			);
 	}
 }
