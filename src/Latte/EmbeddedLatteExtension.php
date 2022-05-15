bd65b6d0a3bf1daaf25801958fc45189f429134b
Tomas Votruba <tomas.vot@gmail.com>
2022-05-15 15:03:02 +0200

    fix array of getTags()

--- src/Latte/EmbeddedLatteExtension.php
+++ src/Latte/EmbeddedLatteExtension.php
@@ -13,6 +13,8 @@ final class EmbeddedLatteExtension extends Extension
     {
         // add macros here :)
         // @see https://github.com/nette/application/commit/7bfe14fd214c728cec1303b7b486b2f1e4dc4c43#diff-f478cae07da9b043d8410bf46671215af5c8ffb8bdd430beb395ed8b63e52ffcR54
-        return ['embeddedSvg', [EmbeddedSvgNode::class, 'create']];
+        return [
+            'embeddedSvg' => [EmbeddedSvgNode::class, 'create']
+        ];
     }
 }
